aa814a9f845ba674db7bb0ad54acc34153661f48
Fausto Leal <fausto.leal2099@gmail.com>
2025-11-04 00:10:59 -0300

    modificando BASE_URL

--- client/hermanos-jota/src/api/productosApi.js
+++ client/hermanos-jota/src/api/productosApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "/api/productos";
+const BASE_URL = import.meta.env.VITE_API_URL || "/api/productos";
 
 export const getProductos = async () => {
   const response = await fetch(BASE_URL, {

b99a69e9e1adc6d0d6a56560e178980c59ceb39b
Fausto Leal <fausto.leal2099@gmail.com>
2025-11-07 15:39:43 -0300

    solucionando problemas de CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,24 @@ const PORT = process.env.PORT || 4000;
 
 connectDB();
 
-app.use(cors());
+const originsList = [
+  "https://hermanos-jota-v2.vercel.app/",
+  "http://localhost:5173",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (originsList.includes(origin)) return callback(null, true);
+    return callback(new Error("Not allowed by CORS"));
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(middleware.logger);

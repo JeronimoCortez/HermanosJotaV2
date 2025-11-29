055ebd59ba389776359f9fb32ccec0751c1c1236
Fausto Leal <fausto.leal2099@gmail.com>
2025-11-29 17:16:55 -0300

    agregando ProductContext

--- client/hermanos-jota/src/context/CartContext.jsx
+++ client/hermanos-jota/src/context/CartContext.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, createContext } from "react";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
